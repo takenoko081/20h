b28393668ab349d1308080299913d470509ce437
take5 <take5@DESKTOP-KP6F44N>
2016-06-09 21:37:11 +0900

    add InputButton and DisplayButton

---
+++ 20h/src/pkg20h/InputPageController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg20h;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author take5
+ */
+public class InputPageController implements Initializable{
+    
+        
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+    
+}
---
+++ 20h/src/pkg20h/TopPageController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg20h;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author take5
+ */
+public class TopPageController implements Initializable {
+
+    @FXML
+    private Button inputPage;
+    @FXML
+    private Button displayPage;
+    
+    @FXML
+    private void handleInputPageButtonAction(ActionEvent event) throws IOException {
+//        FXMLLoader fXMLLoader = new FXMLLoader(getClass().getResource("HelloWorldFXMLDocument.fxml"));
+//        Parent parent = fXMLLoader.load();
+//        Scene s = new Scene(parent);
+//        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+//        stage.setScene(s);
+//        stage.show();
+    }
+
+    @FXML
+    private void handleDisplayPageButtonAction(ActionEvent event) throws IOException {
+        System.out.println("Hello World!!!!!!");
+    }
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
