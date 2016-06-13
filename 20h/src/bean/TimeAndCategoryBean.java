fb0a30e560f2b55526dffefad49c51067cc3eb5a
take5 <take5@DESKTOP-KP6F44N>
2016-06-14 00:42:59 +0900

    add TimeAndCategoryBean class

---
+++ 20h/src/bean/TimeAndCategoryBean.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author take5
+ */
+public class TimeAndCategoryBean {
+    private LocalDateTime localDateTime;
+    private String category;
+
+    public TimeAndCategoryBean(LocalDateTime localDateTime, String category) {
+        this.localDateTime = localDateTime;
+        this.category = category;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+    
+    
+    
+}
