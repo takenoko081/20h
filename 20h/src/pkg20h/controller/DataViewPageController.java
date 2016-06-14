ea461e94a8304a2bd468544a012c421f256fa0dc
take5 <take5@DESKTOP-KP6F44N>
2016-06-14 23:18:46 +0900

    DataViewPageにグラフの追加

--- 20h/src/pkg20h/controller/DataViewPageController.java
+++ 20h/src/pkg20h/controller/DataViewPageController.java
@@ -10,9 +10,12 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
 
 /**
  * FXML Controller class
@@ -21,17 +24,27 @@ import javafx.fxml.Initializable;
  */
 public class DataViewPageController implements Initializable {
 
+    @FXML
+    private BarChart<String, Integer> studyBarChart;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
     }
     
     @FXML
     private void handleDisplayButtonAction(ActionEvent event) throws IOException{
         Files.lines(Paths.get("test.txt")).filter(str -> !str.equals("")).forEach(System.out::println);
+        
+        XYChart.Series<String, Integer> series = new XYChart.Series<String, Integer>();
+        series.setName("H");
+        
+        series.getData().add(new XYChart.Data<String, Integer>("1日", 100));
+        
+        studyBarChart.getData().add(series);
+        
     }
     
 }
