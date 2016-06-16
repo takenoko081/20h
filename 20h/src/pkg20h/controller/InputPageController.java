bd2f4a31258c1d003e96b30cc05bb1fbde0349ce
take5 <take5@DESKTOP-KP6F44N>
2016-06-16 22:25:12 +0900

    InputPageにカテゴリ追加機能を追加した。

--- 20h/src/pkg20h/controller/InputPageController.java
+++ 20h/src/pkg20h/controller/InputPageController.java
@@ -9,12 +9,17 @@ import bean.TimeAndCategoryBean;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.security.KeyStore;
+import java.text.AttributedCharacterIterator;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ResourceBundle;
+import java.util.stream.Stream;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -31,6 +36,7 @@ public class InputPageController implements Initializable{
     @FXML private TextField hour;
     @FXML private TextField minute;
     @FXML private TextField category;
+    @FXML private TextField categoryEntryField;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -45,5 +51,22 @@ public class InputPageController implements Initializable{
         TimeAndCategoryBean bean = new TimeAndCategoryBean(localDateTime, category);
         Files.write(Paths.get(PATH), bean.toCSV().getBytes(), StandardOpenOption.APPEND);
     }
+    
+    @FXML
+    private void handleCategoryEntryButton(ActionEvent event) throws IOException {
+
+        Stream<String> stream = Files.lines(Paths.get("category.txt"));
+        boolean strExists = stream.anyMatch(str -> str.contains(categoryEntryField.getText()));
+        if(strExists){
+            return;
+        }
+        
+        boolean fileExists = Files.exists(Paths.get("category.txt"), LinkOption.NOFOLLOW_LINKS);
+        String category = categoryEntryField.getText() + System.getProperty("line.separator");
+        if(!fileExists) {
+            Files.createFile(Paths.get("category.txt"));
+        }
+        Files.write(Paths.get("category.txt"), category.getBytes(), StandardOpenOption.APPEND);
+    };
     
 }
