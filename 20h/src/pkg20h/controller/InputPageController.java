d481593ed77bab7ebf111d6a9dc4b2d6c4c26dc2
タケノコ <take5@MyComputer>
2016-06-18 14:16:23 +0900

    勉強時間登録時のカテゴリの入力をテキストフィールドからチョイスボックスに変更

--- 20h/src/pkg20h/controller/InputPageController.java
+++ 20h/src/pkg20h/controller/InputPageController.java
@@ -18,12 +18,20 @@ import java.text.AttributedCharacterIterator;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
 /**
@@ -38,9 +46,19 @@ public class InputPageController implements Initializable{
     @FXML private TextField minute;
     @FXML private TextField category;
     @FXML private TextField categoryEntryField;
+    @FXML private ChoiceBox<String> categoryChoiceBox;
+    private List<String> listsCategory;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        try {
+            listsCategory = Files.lines(Paths.get("category.txt")).collect(Collectors.toList());
+        } catch (IOException ex) {
+            Logger.getLogger(InputPageController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        ObservableList<String> lists = FXCollections.observableList(listsCategory);
+        categoryChoiceBox.setItems(lists);        
     }
     
     /**
@@ -48,7 +66,6 @@ public class InputPageController implements Initializable{
      * @param event
      * @throws IOException 
      */
-    
     @FXML
     private void handleWriteFileButtonAction(ActionEvent event) throws IOException {
         int hour = Integer.parseInt(this.hour.getText());
@@ -64,7 +81,6 @@ public class InputPageController implements Initializable{
      * @param event
      * @throws IOException 
      */
-    
     @FXML
     private void handleCategoryEntryButton(ActionEvent event) throws IOException {
 
